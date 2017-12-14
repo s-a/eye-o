20d0d98049696c1259fe0f58dff1bd7d09ccc52f
Stephan Ahlf <stephan.ahlf@googlemail.com>
2017-12-14 21:18:34 +0100

    add styles and move focused filesystem item with <kbd>up</kbd> and <kbd>down</kbd>

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -5,16 +5,17 @@ import SplitPane from 'react-split-pane';
 import Mousetrap from 'mousetrap';
 
 import icons from 'file-icons-js'
-
+import $ from 'jquery'
 
 
 let glob = require('glob');
 
 class Home extends Component {
+
   state = {
     areas: [
       {
-        locations: ['c:/git', 'c:/temp'],
+        locations: ['c:/git', 'c:/git/eye-o'],
         activeIndex: 1,
         contents: {
           directories: [
@@ -28,7 +29,7 @@ class Home extends Component {
         }
       },
       {
-        locations: ['c:/windows', 'c:/windows', 'c:/windows', 'c:/windows'],
+        locations: ['c:/windows', 'c:/windows/de-DE', 'c:/windows', 'c:/windows'],
         activeIndex: 2,
         contents: {
           directories: [
@@ -43,6 +44,37 @@ class Home extends Component {
       }
     ]
   }
+  componentDidMount() {
+    const self = this
+    Mousetrap.bind('down', function (e) {
+      self.focusNextElement()
+      return false;
+    });
+    Mousetrap.bind('up', function (e) {
+      self.focusPreviousElement()
+      return false;
+    });
+    this.changeActiveTabIndex(0, this.state.areas[0], 1)
+    this.changeActiveTabIndex(1, this.state.areas[1], 0)
+
+    $('.file-system-view').css({ 'height': ($(window).height() - 20) + 'px' });
+    $(window).resize(function () { // On resize
+      $('.file-system-view').css({ 'height': ($(window).height() - 20) + 'px' });
+    });
+
+  }
+
+  focusNextElement(current) {
+    //add all elements we want to include in our selection
+    var x = $(document.activeElement)
+    x.next().focus()
+  }
+
+  focusPreviousElement(current) {
+    //add all elements we want to include in our selection
+    var x = $(document.activeElement)
+    x.prev().focus()
+  }
 
   changeActiveTabIndex(areaIndex, area, newActiveIndex) {
     let newState = this.state;
@@ -54,20 +86,24 @@ class Home extends Component {
     let fs = require('fs');
     let getDirs = function (rootDir, cb) {
       fs.readdir(rootDir, (err, files) => {
-        let dirs = [];
+        let dirs = ['..'];
         let filenames = [];
         for (let index = 0; index < files.length; ++index) {
           let file = files[index];
           if (file[0] !== '.') {
             let filePath = `${rootDir}/${file}`;
             fs.stat(filePath, function (err, stat) {
-              if (stat.isDirectory()) {
-                dirs.push(this.file);
+              if (err) {
+                console.warn(`error getting stats of ${file}`)
               } else {
-                filenames.push(this.file);
-              }
-              if (files.length === (this.index + 1)) {
-                return cb({ directories: dirs, files: filenames });
+                if (stat.isDirectory()) {
+                  dirs.push(this.file);
+                } else {
+                  filenames.push(this.file);
+                }
+                if (files.length === (this.index + 1)) {
+                  return cb({ directories: dirs.sort(), files: filenames.sort() });
+                }
               }
             }.bind({ index, file }));
           }
@@ -84,7 +120,7 @@ class Home extends Component {
   renderTabs(areaIndex, area) {
     let tabs = [];
     for (let index = 0; index < area.locations.length; index++) {
-      const location = area.locations[index];
+      const location = area.locations[index].split('/').pop();
       tabs.push(
         <li key={index} className="tab" >
           <span onClick={() => this.changeActiveTabIndex(areaIndex, area, index)} className={index === area.activeIndex ? 'tab-active' : null} >{location}</span>
@@ -103,12 +139,27 @@ class Home extends Component {
     for (let d = 0; d < area.contents.directories.length; d++) {
       const dir = area.contents.directories[d];
       const className = 'folder';
-      files.push(<li key={areaIndex + dir} ><span className={className} /> {dir}</li>);
+      files.push(
+        <a key={areaIndex + dir} className={"filesystem-item"} href="#">
+          <span className={className} /> <span className={'filesystem-item-name dark-yellow'}>{dir}</span>
+        </a>
+      );
     }
     for (let f = 0; f < area.contents.files.length; f++) {
       const fn = area.contents.files[f];
       const className = icons.getClassWithColor(fn);
-      files.push(<li key={areaIndex + fn}><span className={className} /> {fn}</li>);
+      var colorClassName = (className || '').split(" ")
+      if (colorClassName.length === 2) {
+
+        colorClassName = colorClassName.pop()
+      } else {
+        colorClassName = ''
+      }
+      files.push(
+        <a key={areaIndex + fn} className={"filesystem-item"} href="#">
+          <span className={className} /> <span className={'filesystem-item-name ' + colorClassName}>{fn}</span>
+        </a>
+      );
     }
 
     const result = <ul className="area">{files}</ul>;
@@ -126,15 +177,15 @@ class Home extends Component {
         defaultSize={parseInt(localStorage.getItem('splitPos'), 10)}
         onChange={size => localStorage.setItem('splitPos', size)}
       >
-        <div className="file-system-view">
+        <div>
           {this.renderTabs(0, this.state.areas[0])}
-          <div>
+          <div className="file-system-view">
             {this.renderArea(0, this.state.areas[0])}
           </div>
         </div>
-        <div className="file-system-view">
+        <div>
           {this.renderTabs(1, this.state.areas[1])}
-          <div>
+          <div className="file-system-view">
             {this.renderArea(1, this.state.areas[1])}
           </div>
         </div>
