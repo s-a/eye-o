9e1f9d862c7a09ea073df71e43b3ec181e33463b
Stephan Ahlf <stephan.ahlf@googlemail.com>
2017-12-16 22:02:39 +0100

    toggle shell terminal and fit height of file system views

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,18 +1,30 @@
 // @flow
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import Mousetrap from 'mousetrap';
 import Terminal from 'xterm';
 import icons from 'file-icons-js';
 import $ from 'jquery';
-var pty = require('node-pty');
 
+const pty = require('node-pty');
 
-
-var xterm = new Terminal()
-const glob = require('glob');
+const xterm = new Terminal();
 // Notice it's called statically on the type, not an object
 Terminal.loadAddon('fit');
+
+const fitHeight = function fitHeight(el) {
+  let total = $('.terminal-container-header').outerHeight() + 40;
+  if ($('.terminal-container:visible').length !== 0) {
+    total += $('.terminal-container:visible').outerHeight() + 80;
+  }
+
+  $(el).parent().children().not(el)
+    .each(function () {
+      total += $(this).outerHeight();
+    });
+  let newHeight = $(window).height() - total;
+  $(el).height(newHeight)
+}
 
 class Home extends Component {
 
@@ -61,49 +73,51 @@ class Home extends Component {
     this.changeActiveTabIndex(0, this.state.areas[0], 1);
     this.changeActiveTabIndex(1, this.state.areas[1], 0);
 
-    $('.file-system-view').css({ height: `${$(window).height() - 180}px` });
     $(window).resize(() => { // On resize
-      $('.file-system-view').css({ height: `${$(window).height() - 180}px` });
-
+      $('.file-system-view').each(function () {
+        fitHeight(this)
+      })
     });
 
 
-    var terminalContainer = document.getElementById('terminal')
-    var terminalInput = document.getElementById('terminal-input')
-    $(terminalInput).keydown(function (e) {
-      if (e.keyCode == 13) {
-        var txt = terminalInput.value
-        term.write(txt + '\r\n');
+    const terminalContainer = document.getElementById('terminal');
+    const terminalInput = document.getElementById('terminal-input');
 
+    $(terminalInput).keydown((e) => {
+      if (e.keyCode === 13) {
+        const txt = terminalInput.value;
+        term.write(`${txt}\r\n`);
+        terminalInput.value = '';
       }
-    })
+      if (e.keyCode === 27) {
+        self.showTerminal(false)
+      }
+    });
+    $(terminalInput).blur((e) => {
+      self.showTerminal(false)
+    });
 
     xterm.open(terminalContainer);
 
-    setTimeout(() => {
 
-      xterm.fit();
-    }, 200);
-
-    var term = pty.spawn(process.platform === 'win32' ? 'cmd.exe' : 'bash', [], {
+    const term = pty.spawn(process.platform === 'win32' ? 'cmd.exe' : 'bash', [], {
       name: 'xterm-color',
       cols: 180,
       rows: 80,
       cwd: process.env.PWD,
       env: process.env
     });
-    term.on('data', function (data) {
+    term.on('data', (data) => {
       console.log(data);
-      xterm.write(data)
+      xterm.write(data);
     });
 
-    term.on('key', function (key, ev) {
+    term.on('key', (key, ev) => {
+      const printable = (!ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey);
 
-      var printable = (!ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey);
-
-      if (ev.keyCode == 13) {
+      if (ev.keyCode === 13) {
         term.prompt();
-      } else if (ev.keyCode == 8) {
+      } else if (ev.keyCode === 8) {
         // Do not delete the prompt
         if (term.x > 2) {
           term.write('\b \b');
@@ -113,22 +127,47 @@ class Home extends Component {
       }
     });
 
-
     term.write('cd c:\\temp\r\n');
 
+    setTimeout(function () {
+      $('.file-system-view').each(function () {
+        fitHeight(this)
+      })
+    }, 100)
 
+  }
 
+  toggleTerminal() {
+    this.showTerminal(!$('.terminal-container').is(":visible"))
+  }
+
+  showTerminal(show) {
+    const c = $('.terminal-container')
+    const done = function () {
+      $('.file-system-view').each(function () {
+        fitHeight(this)
+        if (c.is(":visible")) {
+          $('#terminal-input').focus()
+          xterm.fit()
+        }
+      })
+    }
+    if (show) {
+      c.fadeIn(0, done);
+    } else {
+      c.fadeOut(0, done);
+    }
   }
 
   focusNextElement(current) {
     // add all elements we want to include in our selection
-    let x = $(document.activeElement);
+    const x = $(document.activeElement);
     x.next().focus();
   }
 
   focusPreviousElement(current) {
     // add all elements we want to include in our selection
-    let x = $(document.activeElement);
+    const x = $(document.activeElement);
     x.prev().focus();
   }
 
@@ -196,9 +235,9 @@ class Home extends Component {
 
     for (let d = 0; d < area.contents.directories.length; d++) {
       const dir = area.contents.directories[d];
-      const className = 'folder';
+      const className = 'folder-icon';
       files.push(
-        <a key={areaIndex + dir} className={'filesystem-item dark-yellow'} href="#">
+        <a key={areaIndex + dir} className={'filesystem-item folder'} href="#">
           <span className={className} /> <span className={'filesystem-item-name'}>{dir}</span>
         </a>
       );
@@ -213,7 +252,7 @@ class Home extends Component {
         colorClassName = '';
       }
       files.push(
-        <a key={areaIndex + fn} className={'filesystem-item ' + colorClassName} href="#">
+        <a key={areaIndex + fn} className={`filesystem-item ${colorClassName}`} href="#">
           <div className="row">
             <div className="col-xs-6">
               <span className={className} /> <span className={'filesystem-item-name'}>{fn}</span>
@@ -253,16 +292,21 @@ class Home extends Component {
           </div>
         </div>
         <div className="row vbottom">
-          <div className="col-sm-6 vbottom">
+          <div className="col-sm-6 ">
             Bottom DIV
           </div>
-          <div className="col-sm-6 vbottom">
+          <div className="col-sm-6 ">
             Bottom DIV
           </div>
         </div>
-        <div className="row vbottom">
+        <div className="row">
+          <div className="col-sm-12 vbottom terminal-container-header">
+            <a href="#" onClick={this.toggleTerminal.bind(this)}>Shell</a>
+          </div>
+        </div>
+        <div className="row vbottom terminal-container">
           <div className="col-sm-12 vbottom">
-            <div id="terminal"></div>
+            <div id="terminal" />
             <input type="text" id="terminal-input" />
           </div>
         </div>
